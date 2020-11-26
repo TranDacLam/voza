2e0b7f2bf71198919898b539538e6ec1789a60a7
TranDacLam <tdlam2105@gmail.com>
2020-11-26 23:13:36 +0700

    #2

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ $(function(){
 		  dynamicBullets: true,
 		},
 	});
+
+
+	$("#navbar-menu .nav-item").hover(function() {
+		var isHovered = $(this).is(":hover");
+		if (isHovered) {
+		  	$(this).children(".nav-menu-sub").stop().slideDown(300);
+		} else {
+		  	$(this).children(".nav-menu-sub").stop().slideUp(300);
+		}
+	});
+	  
 })
